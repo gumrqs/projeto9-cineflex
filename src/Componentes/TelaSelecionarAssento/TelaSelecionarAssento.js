aed24082f0b36e3d45b2346fcaa30db407e6811d
Gustavo <gumrqss@gmail.com>
2022-07-31 13:46:08 -0300

    validacao forms

--- src/Componentes/TelaSelecionarAssento/TelaSelecionarAssento.js
+++ src/Componentes/TelaSelecionarAssento/TelaSelecionarAssento.js
@@ -8,9 +8,24 @@ import SalaAssentos from './SalaAssentos';
 
 export default function TelaSelecionarAssento(){
     const {idSessao} = useParams();
-        console.log(idSessao, "id sessao aqui")
     const[listaAssentos, setListaAssentos]= useState([]);
-   /*  const[urlFooter, setUrlFooter]= useState([]); */
+    const[nome, setNome]= useState('');
+    const[cpf, setCpf]= useState('');
+
+   function handleForm(e){
+       e.preventDefault();
+       const body={
+           nome,
+           cpf
+       };
+       console.log(body, "o body aqui")
+
+       if(cpf.length<14 || cpf.length>14)
+       {
+           alert("Digite um cpf válido: xxx.xxx.xxx-xx");
+       }
+
+   }
     
 
 	useEffect(() => {
@@ -36,22 +51,48 @@ export default function TelaSelecionarAssento(){
             {listaAssentos.map((assento,index)=> <SalaAssentos key={index} situacao={assento.isAvailable} assento={assento.name}/>)}
             </div>
             <div className='legenda'>
-                <div className='circ1'>
+                <div className='circ'>
                     <div className='disponivel'>
                     </div>
                     <p>Selecionado</p>
                 </div>
-                <div className='circ1'>
+                <div className='circ'>
                     <div className='reservada'>
                     </div>
                     <p>Disponível</p>
                 </div>
-                <div className='circ1'>
+                <div className='circ'>
                     <div className='indisponivel'>
                     </div>
                     <p>Indisponível</p>
                 </div>
-
+            </div>
+            <div className='formulario'>
+                <form onSubmit={handleForm}>
+                    <div className='dados'>
+                        <div className='input-formulario'>
+                            <div className='titulo'>
+                                <span>Nome do comprador</span>
+                                <input type="text" onChange={(e) => setNome(e.target.value)} 
+                                    value={nome}
+                                    required
+                                    placeholder='Digite seu nome...'
+                                />
+                            </div>
+                        </div>
+                        <div className='input-formulario'>
+                            <div className='titulo'>
+                                <span>CPF do comprador</span>
+                                <input type="text" onChange={(e) => setCpf(e.target.value)}
+                                    value={cpf}
+                                    required
+                                    placeholder='Digite seu CPF...'
+                                />
+                            </div>
+                        </div>
+                        <button className='guardar-dados'> <p>Reservar assento(s)</p></button>
+                    </div>
+                </form>
             </div>
         </>
     )
